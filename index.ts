b894fb4269a7e2dbced67c69f7e9ff78dc0cb7af
lmssee <earthnut.dev@outlook.com>
2025-08-17 01:54:35 +0800

    version : v0.1.3 2025/8/17 01:54:35

--- index.ts
+++ index.ts
@@ -10,15 +10,15 @@ import {
 /**
  * 参数的数据类型
  */
-type ClassNameItem =
+export type EnClassNameItem =
   | null
   | number
   | string
   | boolean
   | undefined
   | (() => string)
-  | (() => ClassNameItem[])
-  | ClassNameItem[]
+  | (() => EnClassNameItem[])
+  | EnClassNameItem[]
   | Record<string, boolean | undefined>;
 
 /**
@@ -26,12 +26,12 @@ type ClassNameItem =
  *
  * 字符串为具体的字符串值而非 string
  */
-type TypeofClassNameItem<T> = T extends null | boolean | undefined
+export type EnTypeofClassNameItem<T> = T extends null | boolean | undefined
   ? ''
   : T extends readonly [unknown, infer U]
-    ? TypeofClassNameItem<U> | ''
+    ? EnTypeofClassNameItem<U> | ''
     : T extends readonly [unknown, infer U, infer V]
-      ? TypeofClassNameItem<U> | TypeofClassNameItem<V>
+      ? EnTypeofClassNameItem<U> | EnTypeofClassNameItem<V>
       : T extends Record<string, boolean | undefined>
         ? keyof T | ''
         : T extends () => string
@@ -41,7 +41,9 @@ type TypeofClassNameItem<T> = T extends null | boolean | undefined
 /**
  * 递归判断当前返回的数据类型
  */
-type XCN<T> = T extends [infer U, ...infer V] ? `${U & string}  ${XCN<V>}` : '';
+export type EnXcn<T> = T extends [infer U, ...infer V]
+  ? `${U & string}  ${EnXcn<V>}`
+  : '';
 
 /**
  *
@@ -50,9 +52,9 @@ type XCN<T> = T extends [infer U, ...infer V] ? `${U & string}  ${XCN<V>}` : '';
  * merge class name
  *
  */
-export function xcn<T extends ClassNameItem[]>(
+export function xcn<T extends EnClassNameItem[]>(
   ...classNameList: T
-): XCN<{ [K in keyof T]: TypeofClassNameItem<T[K]> }> {
+): EnXcn<{ [K in keyof T]: EnTypeofClassNameItem<T[K]> }> {
   /**  临时  */
   const template: string[] = [];
   /**  移除空白  */
@@ -96,7 +98,7 @@ export function xcn<T extends ClassNameItem[]>(
   });
   const result = removeBlank(new Array(...new Set(template)).join(' '));
 
-  return (result ?? undefined) as unknown as XCN<{
-    [K in keyof T]: TypeofClassNameItem<T[K]>;
+  return (result ?? undefined) as unknown as EnXcn<{
+    [K in keyof T]: EnTypeofClassNameItem<T[K]>;
   }>;
 }
